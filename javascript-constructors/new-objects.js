fa159fb2fe021a2fec3342b9c733385e5b011232
ChiaYu Lin <lin.1008@osu.edu>
2021-06-22 20:59:21 -0700

    Completed javascript-constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,29 @@
+// Object
+var anObject = new Object();
+console.log('value of anObject:', anObject);
+console.log('typeof anObject:', typeof anObject);
+
+//Array
+var anArray = new Array();
+console.log('value of anArray:', anArray);
+console.log('typeof anArray:', typeof anArray);
+
+//Function
+var anFunction = new Function();
+console.log('value of anFunction:', anFunction);
+console.log('typeof anFunction:', typeof anFunction);
+
+//String
+var anString = new String();
+console.log('value of anString:', anString);
+console.log('typeof anString:', typeof anString);
+
+//Number
+var anNumber = new Number();
+console.log('value of anNumber:', anNumber);
+console.log('typeof anNumber:', typeof anNumber);
+
+//Boolean
+var anBoolean = new Boolean();
+console.log('value of anBoolean:', anBoolean);
+console.log('typeof anBoolean:', typeof anBoolean);
